d709321de36735b57d8f51a31f563b11c9757bb8
santoshpatil1512 <159259680+santoshpatil1512@users.noreply.github.com>
2024-02-19 20:56:47 +0530

    Second commit

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ require('dotenv').config();
 app.use(express.json());
 const PORT = process.env.PORT || 3000
 
+app.get('/home', (req, res)=>{
+    res.json('Welcome to home home');
+});
+
+app.get('/about', (req, res)=>{
+    res.json('Welcome to about page');
+});
+
 app.use('/emps', employeeRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
-})  
+});  
